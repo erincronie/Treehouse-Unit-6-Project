8bd1737fa020bf22966fbdf68ff2770faa84f47a
Erin Cronie <erincronie@gmail.com>
2022-06-10 08:56:02 -0700

    added eventlisteners

---
+++ app.js
@@ -0,0 +1,51 @@
+const qwerty = document.querySelector('#qwerty');
+const phrase = document.querySelector('#phrase ul');
+const phrases = [
+'Single Ladies',
+'Shake it Off',
+'Rolling in the Deep',
+'Hips dont Lie',
+'Poker Face'
+];
+
+const startBtn = document.querySelector('.btn__reset');
+const overlay = document.querySelector('#overlay');
+
+ let missed = 0; 
+
+ btnReset.addEventListener('click', e => {
+    overlay.style.display = "none"
+});
+
+//return a random phrase from an array
+const getRandomPhraseAsArray = arr => {
+
+
+}
+
+//add the letters of a string to the display
+const addPhraseToDisplay = arr => {
+
+
+}
+
+
+//check if a letter is in the phrase
+constCheckLetter = button => {
+
+
+}
+
+
+//listen for the start game button to be pressed
+startButton.addEventListener('click', () => {
+
+
+});
+
+//listen for the onscreen keyboard to be clicked
+qwerty.addEventListener ('click', e => {
+
+
+
+});

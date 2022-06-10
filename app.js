26c6b054919fc27b00a54cdde82cc343d7a4dd09
Erin Cronie <erincronie@gmail.com>
2022-06-10 14:42:57 -0700

    added up to letter checker section

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const qwerty = document.querySelector('#qwerty');
-const phrase = document.querySelector('#phrase ul');
-const phrases = [
+const startButton = document.querySelector('.btn__reset');
+const overlay = document.querySelector('.start');
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+
+let missed = 0; 
+let phrases = [
 'Single Ladies',
 'Shake it Off',
 'Rolling in the Deep',
@@ -8,44 +12,50 @@ const phrases = [
 'Poker Face'
 ];
 
-const startBtn = document.querySelector('.btn__reset');
-const overlay = document.querySelector('#overlay');
-
- let missed = 0; 
-
- btnReset.addEventListener('click', e => {
-    overlay.style.display = "none"
-});
-
-//return a random phrase from an array
-const getRandomPhraseAsArray = arr => {
-
-
-}
-
-//add the letters of a string to the display
-const addPhraseToDisplay = arr => {
-
-
-}
-
-
-//check if a letter is in the phrase
-constCheckLetter = button => {
-
-
-}
-
+const phraseArray = getRandomPhraseAsArray(phrases);
 
 //listen for the start game button to be pressed
-startButton.addEventListener('click', () => {
+ startButton.addEventListener('click', e => {
+    overlay.style.display = 'none'});
+
+//return a random phrase from an array
+function getRandomPhraseAsArray(arr) {
+    let random = Math.floor(Math.random() * arr.length);
+    let randomPhrase = arr[random].toLowerCase();
+    let newPhrase = randomPhrase.split('');
+    return newPhrase;
+}
+
+//adds the letters of a string to the display
+function addPhraseToDisplay(arr) {
+    for (let i = 0; i < arr.length; i++ ) { 
+        const ul = document.querySelector('ul');
+        const li = document.createElement('li');
+        li.textContent = arr[i];
+        ul.appendChild(li);
+        if (li.textContent !== ' ') {
+            li.className = 'letter';
+        } else {
+            li.className = 'space';
+        }
+    }
+    //return arr; - may not need this?
+}
+addPhraseToDisplay(phraseArray);
 
 
-});
+//check if a letter is in the phrase - check letter function
+function checkLetter( button )  {
+    const letters = document.querySelectorAll('.letter');
+    let match = null;
+    let selectLi = document.querySelectorAll('li')
+    for ( let i = 0; 1 < letters.length; i++ ){
+        if ( button.textContent === selectLi[li].textContent.toLowerCase() ){
+            selectLi[i].classList.add('show');
+            match = button.textContent;
+        }
+    }
+return match;
+}
 
 //listen for the onscreen keyboard to be clicked
-qwerty.addEventListener ('click', e => {
-
-
-
-});
